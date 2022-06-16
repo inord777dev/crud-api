318ff51a46422fced4c591879cd2db8405528915
inord777dev <inord777dev@gmail.com>
2022-06-16 11:40:03 +0300

    feat: api complete, cluster complete

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import * as http from 'http';
 import { randomUUID } from 'crypto';
+import cluster from 'cluster';
+import { cpus } from 'os';
 
 const port = Number(process.env.PORT) || 3000;
 const hostname = '127.0.0.1';
@@ -13,6 +15,7 @@ interface IUser {
 }
 
 const users: IUser[] = [];
+
 const STATUS_OK = 200;
 const STATUS_CREATE = 201;
 const STATUS_DELETE = 204;
@@ -30,75 +33,130 @@ const writeError = (response: http.ServerResponse, statusCode: number, message: 
 }
 
 const writeResult = (response: http.ServerResponse, statusCode: number, data: any) => {
-  response.writeHead(STATUS_OK, headers);
+  response.writeHead(statusCode, headers);
   response.end(JSON.stringify(data));
 }
 
 const isUUID = (uuid: string) => {
-  return /^[0-9A-F]{8}-[0-9A-F]{4}-[1][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i.test(uuid);
+  return /^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/i.test(uuid);
 }
 
-const parseData = (body: string) => {
-  let obj;
+const parseUser = (body: string, response: http.ServerResponse) => {
+  let user: IUser | undefined;
   try {
-    obj = JSON.parse(body);
+    user = JSON.parse(body) as IUser;
   }
   catch {
   }
-  return obj;
+
+  if (typeof user?.username === 'string'
+    && typeof user?.age === 'number'
+    && Array.isArray(user?.hobbies) && user.hobbies.every((x) => typeof x === 'string')) {
+    return user;
+  }
+  else {
+    writeError(response, STATUS_INPUT_INVALID, 'Input data invalid');
+  }
 }
 
-const server = http.createServer((request, response) => {
-  const { pathname } = new URL(request.url as string, `http://${request.headers.host}`);
-  try {
-    if (pathname === '/api/users' && request.method === 'GET') {
-      writeResult(response, STATUS_OK, users);
+const findUserIndex = (pathname: string, response: http.ServerResponse) => {
+  const uuid = pathname.replace('/api/users/', '');
+  let index = -1;
+  if (isUUID(uuid)) {
+    index = users.findIndex((x) => uuid === x.id);
+    if (index === -1) {
+      writeError(response, STATUS_NOT_FOUND, 'User not found');
     }
-    else if (pathname === '/api/users' && request.method === 'POST') {
-      let body = "";
+  }
+  else {
+    writeError(response, STATUS_INPUT_INVALID, 'UUID invalid');
+  }
+  return index;
+}
 
-      request.on("data", function (chunk) {
-        body += chunk;
-      });
+const startServer = () => {
+  const server = http.createServer((request, response) => {
+    const { pathname } = new URL(request.url as string, `http://${request.headers.host}`);
+    try {
+      if (pathname === '/api/users' && request.method === 'GET') {
+        writeResult(response, STATUS_OK, users);
+      }
+      else if (pathname.startsWith('/api/users/') && request.method === 'GET') {
+        const userIndex = findUserIndex(pathname, response);
+        if (userIndex !== -1) {
+          writeResult(response, STATUS_OK, users[userIndex]);
+        }
+      }
+      else if (pathname === '/api/users' && request.method === 'POST') {
+        let body = "";
+        request.on("data", function (chunk) {
+          body += chunk;
+        });
 
-      request.on("end", function () {
-        const user = parseData(body) as IUser;
-        if (typeof user?.username === 'string'
-          && typeof user?.age === 'number'
-          && Array.isArray(user?.hobbies) && user.hobbies.every((x) => typeof x === 'string')) {
-          user.id = randomUUID();
-          users.push(user);
-          writeResult(response, STATUS_CREATE, user);
+        request.on("end", function () {
+          const user = parseUser(body, response);
+          if (user) {
+            user.id = randomUUID();
+            users.push(user);
+            writeResult(response, STATUS_CREATE, user);
+          }
+        });
+      }
+      else if (pathname.startsWith('/api/users/') && request.method === 'DELETE') {
+        const userIndex = findUserIndex(pathname, response);
+        if (userIndex !== -1) {
+          const user = users[userIndex];
+          users.splice(userIndex, 1);
+          writeResult(response, STATUS_DELETE, user);
         }
-        else {
-          writeError(response, STATUS_INPUT_INVALID, 'Input data invalid');
-        }
-      });
-    }
-    else if (pathname.startsWith('/api/users/') && request.method === 'GET') {
-      const uuid = pathname.replace('/api/users/', '');
-      if (isUUID(uuid)) {
-        const user = users.find((x) => uuid === x.id);
-        if (user) {
-          writeResult(response, STATUS_OK, user);
-        }
-        else {
-          writeError(response, STATUS_NOT_FOUND, 'User not found');
-        }
+      }
+      else if (pathname.startsWith('/api/users/') && request.method === 'PUT') {
+        let body = "";
+        request.on("data", function (chunk) {
+          body += chunk;
+        });
+
+        request.on("end", function () {
+          const userIndex = findUserIndex(pathname, response);
+          if (userIndex !== -1) {
+            const user = parseUser(body, response);
+            if (user) {
+              users[userIndex].username = user.username;
+              users[userIndex].age = user.age;
+              users[userIndex].hobbies = user.hobbies;
+              writeResult(response, STATUS_OK, users[userIndex]);
+            }
+          }
+        });
       }
       else {
-        writeError(response, STATUS_INPUT_INVALID, 'UUID invalid');
+        writeError(response, STATUS_NOT_FOUND, 'Endpoint not found');
       }
     }
-    else {
-      writeError(response, STATUS_NOT_FOUND, 'Endpoint not found');
+    catch {
+      writeError(response, STATUS_FAIL, 'Server side error');
     }
-  }
-  catch {
-    writeError(response, STATUS_FAIL, 'Server side error');
-  }
-})
+  }).listen(port, hostname);
+}
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+
+if (cluster.isPrimary) {
+  if (process.argv.includes('--multi')) {
+    console.log(`Cluster running at http://${hostname}:${port}/`);
+    const numCPUs = cpus().length;
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+    cluster.on('exit', (worker, code, signal) => {
+      console.log(`Worker ${worker.process.pid} died`);
+    });
+  }
+  else {
+    console.log(`Server running at http://${hostname}:${port}/`);
+    startServer();
+  }
+} else {
+  startServer();
+  console.log(`Worker ${process.pid} started`);
+}
+
